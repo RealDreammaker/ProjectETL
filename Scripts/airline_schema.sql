d208c2fd31f9f012cd31a26723f34c2ac1d3794b
MSufra <matthewsufra@gmail.com>
2022-02-06 18:29:03 +1100

    sql update

--- Scripts/airline_schema.sql
+++ Scripts/airline_schema.sql
@@ -2,43 +2,36 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-SET XACT_ABORT ON
+CREATE TABLE "Airlines" (
+    "Airline_id" int   NOT NULL,
+    "Airline" varchar(100)   NOT NULL,
+    "Alias" varchar(100)   NULL,
+    "IATA" varchar(4)   NULL,
+    "ICAO" varchar(7)   NULL,
+    "Callsign" varchar(100)   NULL,
+    "Country" varchar(100)   NULL,
+    "Active" boolean   NOT NULL,
+    CONSTRAINT "pk_Airlines" PRIMARY KEY (
+        "Airline_id"
+     )
+);
 
-BEGIN TRANSACTION QUICKDBD
+CREATE TABLE "Operations" (
+    "id" int   NOT NULL,
+    "Month_Year" varchar(6)   NOT NULL,
+    "Airline_id" int   NOT NULL,
+    "Port_Country" varchar(100)   NOT NULL,
+    "Passengers_In" int   NOT NULL,
+    "Freight_In_tonnes" numeric(7,3)   NOT NULL,
+    "Mail_In_tonnes" numeric(6,3)   NOT NULL,
+    "Passengers_Out" int   NOT NULL,
+    "Freight_Out_tonnes" numeric(7,3)   NOT NULL,
+    "Mail_Out_tonnes" numeric(7,3)   NOT NULL,
+    CONSTRAINT "pk_Operations" PRIMARY KEY (
+        "id"
+     )
+);
 
-CREATE TABLE [Airlines] (
-    [Airline_id] int  NOT NULL ,
-    [Airline] varchar(100)  NOT NULL ,
-    [Alias] varchar(100)  NOT NULL ,
-    [IATA] varchar(2)  NULL ,
-    [ICAO] varchar(3)  NOT NULL ,
-    [Callsign] varchar(100)  NOT NULL ,
-    [Country] varchar(100)  NOT NULL ,
-    [Active] boolean  NOT NULL ,
-    CONSTRAINT [PK_Airlines] PRIMARY KEY CLUSTERED (
-        [Airline_id] ASC
-    )
-)
+ALTER TABLE "Operations" ADD CONSTRAINT "fk_Operations_Airline_id" FOREIGN KEY("Airline_id")
+REFERENCES "Airlines" ("Airline_id");
 
-CREATE TABLE [Operations] (
-    [id] int  NOT NULL ,
-    [Month_Year] date  NOT NULL ,
-    [Airline_id] int  NOT NULL ,
-    [Port_Country] varchar(100)  NOT NULL ,
-    [Passangers_In] int  NOT NULL ,
-    [Freight_In_tonnes] numeric(7,3)  NOT NULL ,
-    [Mail_In_tonnes] numeric(6,3)  NOT NULL ,
-    [Passengers_Out] int  NOT NULL ,
-    [Freight_Out_tonnes] numeric(7,3)  NOT NULL ,
-    [Mail_Out_tonnes] numeric(7,3)  NOT NULL ,
-    CONSTRAINT [PK_Operations] PRIMARY KEY CLUSTERED (
-        [id] ASC
-    )
-)
-
-ALTER TABLE [Operations] WITH CHECK ADD CONSTRAINT [FK_Operations_Airline_id] FOREIGN KEY([Airline_id])
-REFERENCES [Airlines] ([Airline_id])
-
-ALTER TABLE [Operations] CHECK CONSTRAINT [FK_Operations_Airline_id]
-
-COMMIT TRANSACTION QUICKDBD
